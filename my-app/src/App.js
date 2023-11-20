792857d936a6d252f2baa543adc8c6a2cf2dd3d6
김규민 <82032508+wbhaao@users.noreply.github.com>
2023-11-20 19:24:46 +0900

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Score from "./pages/Score";
+import NotFound from "./NotFound";
+
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/product" element={<Login />}></Route>
+          <Route path="/product" element={<Score />}></Route>
+          {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
